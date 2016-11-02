56dc82ce0ad84375667994a19e868b4c28d78f9a
Joseph Kim <josephjkim@gmail.com>
2016-11-01 19:11:58 -0700

    Adds grid and centering

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
-    <h3>Examples page</h3>
+    <div>
+      <h1 className="text-centered">Examples page</h1>
+      <p>Here are few examples to try out</p>
+      <ol>
+        <li>
+          <Link to='/?location=Palo Alto'>Palo Alto, CA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Seoul'>Seoul, Korea</Link>
+        </li>
+      </ol>
+    </div>
   );
 };
 
